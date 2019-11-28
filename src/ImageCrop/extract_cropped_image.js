55331f8f33d907324555a7bce18196b35c76a236
Berend de Boer <berend@pobox.com>
2019-11-29 10:36:06 +1300

    Export function so we can use it inside webpack

--- src/ImageCrop/extract_cropped_image.js
+++ src/ImageCrop/extract_cropped_image.js
@@ -6,17 +6,19 @@
  * Thanks: https://yellowpencil.com/blog/cropping-images-with-javascript/
  */
 function image_crop_cropped_image (data) {
-  var tnCanvas = document.createElement('canvas');
-  var tnCanvasContext = tnCanvas.getContext('2d');
-  tnCanvas.width = data.destination_width;
-  tnCanvas.height = data.destination_height;
-  var bufferCanvas = document.createElement('canvas');
-  var bufferContext = bufferCanvas.getContext('2d');
+  var tnCanvas = document.createElement('canvas')
+  var tnCanvasContext = tnCanvas.getContext('2d')
+  tnCanvas.width = data.destination_width
+  tnCanvas.height = data.destination_height
+  var bufferCanvas = document.createElement('canvas')
+  var bufferContext = bufferCanvas.getContext('2d')
   var imgObj = document.getElementById(data.image_id)
-  bufferCanvas.width = data.image_width;
-  bufferCanvas.height = data.image_height;
-  bufferContext.drawImage(imgObj, 0, 0);
-  tnCanvasContext.drawImage(bufferCanvas, data.left, data.top, data.width, data.height, 0, 0, data.destination_width, data.destination_height);
-  var url = tnCanvas.toDataURL(data.mime_type, data.quality);
-  return url;
+  bufferCanvas.width = data.image_width
+  bufferCanvas.height = data.image_height
+  bufferContext.drawImage(imgObj, 0, 0)
+  tnCanvasContext.drawImage(bufferCanvas, data.left, data.top, data.width, data.height, 0, 0, data.destination_width, data.destination_height)
+  var url = tnCanvas.toDataURL(data.mime_type, data.quality)
+  return url
 }
+
+export { image_crop_cropped_image }
